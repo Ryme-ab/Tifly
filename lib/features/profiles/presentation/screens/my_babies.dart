5f55c5110b16291967044e042e68a07021ac338d
ryme ait belkacem <ryme.aitbelkacem@ensia.edu.dz>
2025-12-04 20:10:53 +0100

    Save my local changes before pulling

---
+++ lib/features/profiles/presentation/screens/my_babies.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../cubit/baby_cubit.dart';
+import '../cubit/baby_state.dart';
+import 'baby_card.dart';
+
+class MyBabiesPage extends StatelessWidget {
+  const MyBabiesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<BabyCubit, BabyState>(
+        builder: (context, state) {
+          if (state is BabyLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (state is BabyLoaded) {
+            return Column(
+              children: [
+                const SizedBox(height: 40),
+                Wrap(
+                  spacing: 20,
+                  runSpacing: 20,
+                  children: state.babies
+                      .map(
+                        (baby) => BabyCard(
+                          name: baby.name,
+                          age: baby.age,
+                          imageUrl: baby.imageUrl,
+                          borderColor: Colors.pinkAccent,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ],
+            );
+          }
+
+          if (state is BabyError) {
+            return Center(child: Text(state.message));
+          }
+
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'package:tifli/core/config/supabaseClient.dart';
-import 'package:tifli/features/auth/presentation/cubit/signin_cubit.dart';
-import 'package:tifli/features/auth/data/repositories/signin_repository.dart';
-import 'package:tifli/features/auth/presentation/screens/signin_screen.dart';
+import 'package:tifli/features/profiles/data/data_sources/baby_remote_data_source.dart';
+import 'package:tifli/features/profiles/data/repositories/baby_repo.dart';
+import 'package:tifli/features/profiles/presentation/cubit/baby_cubit.dart';
+import 'package:tifli/features/profiles/presentation/screens/create_baby_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ Initialize Supabase
   await SupabaseClientManager.initialize();
 
-  runApp(
-    MultiBlocProvider(
+  final supabase = SupabaseClientManager().client;
+
+  runApp(MyApp(supabase: supabase));
+}
+
+class MyApp extends StatelessWidget {
+  final SupabaseClient supabase;
+  const MyApp({super.key, required this.supabase});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
       providers: [
-        BlocProvider<AuthCubit>(
+        BlocProvider<BabyCubit>(
           create: (_) =>
-              AuthCubit(AuthRepository(SupabaseClientManager().client)),
+              BabyCubit(BabyRepository(BabyRemoteDataSource(supabase))),
         ),
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const SignUpPage(),
+        home: AddBabyPage(),
       ),
-    ),
-  );
+    );
+  }
 }
